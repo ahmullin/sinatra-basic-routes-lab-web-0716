1052f6ad3730bafbd6a32a8085e5f010f3bfe487
Alison <alisonhmullin@gmail.com>
2016-08-11 14:18:29 -0400

    made three routes

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Ali!"
+  end
+
+  get '/hometown' do
+    "My hometown is Boulder, CO!"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Fast Car"
+  end
+
 end
